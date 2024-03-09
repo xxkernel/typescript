0b8cc671fc446c4a6306cf364081643ad08324b1
HiyoRitsu <122852098+HiyoRitsu@users.noreply.github.com>
2024-03-09 20:00:15 +0600

    basics 2.0

--- basics_1/type_assertion.ts
+++ basics_1/type_assertion.ts
@@ -1,2 +1,9 @@
 const header = document.getElementById('header') as HTMLElement;
 header.innerText = 'Hello Typescript!';
+
+let tomas = new Employee('Tom', 'Microsoft');
+if (tomas instanceof Person) {
+  console.log('Tom is a Person');
+} else {
+  console.log('Tom is not a Person');
+}
