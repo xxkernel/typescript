14871d939502193221768cde4f11fe8a7582e075
HiyoRitsu <122852098+HiyoRitsu@users.noreply.github.com>
2024-03-09 20:01:22 +0600

    tasks

--- course_materials/5 - Typescript Классы/73 - index.ts
+++ course_materials/5 - Typescript Классы/73 - index.ts
@@ -1,40 +1,62 @@
 enum TransferStatus {
-	Pending = "pending",
-	Rejected = "rejected",
-	Completed = "completed",
+  Pending = 'pending',
+  Rejected = 'rejected',
+  Completed = 'completed',
 }
 
 enum ErrorMessages {
-	NotFound = "Not found: 404",
-	NotEnoughSpace = "Not enough space: 507",
-	Forbidden = "Forbidden: 403",
+  NotFound = 'Not found: 404',
+  NotEnoughSpace = 'Not enough space: 507',
+  Forbidden = 'Forbidden: 403',
 }
 
 interface ITransfer {
-	path: string;
-	data: string[];
-	date?: Date;
-	start: (p: string, d: string[]) => string;
-	stop: (reason: string) => string;
+  path: string;
+  data: string[];
+  date?: Date;
+  start: (p: string, d: string[]) => string;
+  stop: (reason: string) => string;
 }
 
 interface TransferError {
-	message: ErrorMessages;
+  message: ErrorMessages;
 }
 
 // Класс должен имплементировать ITransfer и TransferError
-class SingleFileTransfer {
+class SingleFileTransfer implements ITransfer, TransferError {
+  path: string;
+  data: string[];
+  date: Date;
+  message: ErrorMessages;
+  transferStatus: TransferStatus;
 
-    // Место для реализаций
+  constructor(status: TransferStatus) {
+    this.transferStatus = status;
+  }
+  // Место для реализаций
+  start(p: string, d: string[]) {
+    return 'Transfer started';
+  }
 
-    // Необходимо создать метод checkTransferStatus, проверяющий состояние передачи данных
-    // Можно вывести в консоль данные, можно вернуть строку
+  checkTransferStatus = (): string => {
+    return this.transferStatus;
+  };
 
-    // Необходимо создать метод, который будет останавливать передачу данных
-    // И возвращать строку с причиной и датой остановки (Дата в любом формате)
+  stop = (reason: string) => {
+    return `Transfer stopped, reason: ${reason}, Date: ${new Date().toLocaleString()}`;
+  };
 
-    // Необходимо создать метод, который будет возвращать строку, содержащую
-    // Статус передачи и любое сообщение об ошибке. На ваш выбор или отталкиваться от приходящего аргумента
-    // Метод может показаться странным, но может использоваться для тестов, например
+  makeError = (): string => {
+    // Ведь при ошибке статус всегда "отклонено", правда?
+    return `Status: ${TransferStatus.Rejected}, error message: ${ErrorMessages.Forbidden}`;
+  };
+  // Необходимо создать метод checkTransferStatus, проверяющий состояние передачи данных
+  // Можно вывести в консоль данные, можно вернуть строку
 
+  // Необходимо создать метод, который будет останавливать передачу данных
+  // И возвращать строку с причиной и датой остановки (Дата в любом формате)
+
+  // Необходимо создать метод, который будет возвращать строку, содержащую
+  // Статус передачи и любое сообщение об ошибке. На ваш выбор или отталкиваться от приходящего аргумента
+  // Метод может показаться странным, но может использоваться для тестов, например
 }
