67b34dee1643aa963d5e10ed2ccc2bb9900f446a
HiyoRitsu <122852098+HiyoRitsu@users.noreply.github.com>
2024-03-11 12:01:58 +0600

    tasks

--- course_materials/4 - Typescript Generics and type manipulations/49 - index.ts
+++ course_materials/4 - Typescript Generics and type manipulations/49 - index.ts
@@ -1,24 +1,29 @@
 // Создать Generic-интерфейс PlayerData, который подходил бы для создания таких объектов:
+interface PlayerData<T, S> {
+  game: T;
+  hours: S;
+  server: string;
+}
 
-const player1 = {
-	game: "CS:GO",
-	hours: 300,
-	server: "basic",
+const player1: PlayerData<string, number> = {
+  game: 'CS:GO',
+  hours: 300,
+  server: 'basic',
 };
 
-const player2 = {
-	game: 2048,
-	hours: "300 h.",
-	server: "arcade",
+const player2: PlayerData<number, string> = {
+  game: 2048,
+  hours: '300 h.',
+  server: 'arcade',
 };
 
-const player3 = {
-	game: "Chess",
-	hours: {
-		total: 500,
-		inMenu: 50,
-	},
-	server: "chess",
+const player3: PlayerData<string, object> = {
+  game: 'Chess',
+  hours: {
+    total: 500,
+    inMenu: 50,
+  },
+  server: 'chess',
 };
 
 // Массив данных с фигурами содержит объекты, у каждого из которых обязательно есть свойство name
@@ -31,46 +36,82 @@ const player3 = {
 // { squares: 3, circles: 2, triangles: 2, others: 1 }
 
 interface AmountOfFigures {
-	squares: number;
-	circles: number;
-	triangles: number;
-	others: number;
+  squares: number;
+  circles: number;
+  triangles: number;
+  others: number;
 }
 
-function calculateAmountOfFigures(figure): AmountOfFigures {}
+enum FigureNames {
+  Rect = 'rect',
+  Circle = 'circle',
+  Triangle = 'triangle',
+  Line = 'line',
+}
+
+interface Figure {
+  name: FigureNames;
+}
+
+function calculateAmountOfFigures<T extends Figure>(
+  figure: T[]
+): AmountOfFigures {
+  const amount: AmountOfFigures = {
+    squares: 0,
+    circles: 0,
+    triangles: 0,
+    others: 0,
+  };
+  figure.forEach((fig) => {
+    switch (fig.name) {
+      case FigureNames.Rect:
+        amount.squares++;
+        break;
+      case FigureNames.Circle:
+        amount.circles++;
+        break;
+      case FigureNames.Triangle:
+        amount.triangles++;
+        break;
+      default:
+        amount.others++;
+    }
+  });
+  return amount;
+}
 
 const data = [
-	{
-		name: "rect",
-		data: { a: 5, b: 10 },
-	},
-	{
-		name: "rect",
-		data: { a: 6, b: 11 },
-	},
-	{
-		name: "triangle",
-		data: { a: 5, b: 10, c: 14 },
-	},
-	{
-		name: "line",
-		data: { l: 15 },
-	},
-	{
-		name: "circle",
-		data: { r: 10 },
-	},
-	{
-		name: "circle",
-		data: { r: 5 },
-	},
-	{
-		name: "rect",
-		data: { a: 15, b: 7 },
-	},
-	{
-		name: "triangle",
-	},
+  {
+    name: FigureNames.Rect,
+    data: { a: 5, b: 10 },
+  },
+  {
+    name: FigureNames.Rect,
+    data: { a: 6, b: 11 },
+  },
+  {
+    name: FigureNames.Triangle,
+    data: { a: 5, b: 10, c: 14 },
+  },
+  {
+    name: FigureNames.Line,
+    data: { l: 15 },
+  },
+  {
+    name: FigureNames.Circle,
+    data: { r: 10 },
+  },
+  {
+    name: FigureNames.Circle,
+    data: { r: 5 },
+  },
+  {
+    name: FigureNames.Rect,
+    data: { a: 15, b: 7 },
+  },
+  {
+    name: FigureNames.Triangle,
+  },
 ];
 
 console.log(calculateAmountOfFigures(data));
