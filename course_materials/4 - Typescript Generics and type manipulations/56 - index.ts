2e071ded3e89ccb0b965532957bca1e40e244c5b
HiyoRitsu <122852098+HiyoRitsu@users.noreply.github.com>
2024-03-17 15:03:36 +0600

    tasks

--- course_materials/4 - Typescript Generics and type manipulations/56 - index.ts
+++ course_materials/4 - Typescript Generics and type manipulations/56 - index.ts
@@ -1,57 +1,72 @@
 interface IPhone {
-	company: string;
-	number: number;
+  company: string;
+  number: number;
 }
 
 // IMobilePhone должен наследоваться от IPhone,
 // тип свойства companyPartner зависит от свойства company
 
-interface IMobilePhone {
-	size: string;
-	companyPartner: "same type as company in Phone";
-	manufactured: Date;
+interface IMobilePhone extends IPhone {
+  size: string;
+  companyPartner: IPhone['company'];
+  manufactured: Date;
 }
 
 // Типизировать объект phones
 
-const phones = [
-	{
-		company: "Nokia",
-		number: 1285637,
-		size: "5.5",
-		companyPartner: "MobileNokia",
-		manufactured: new Date("2022-09-01"),
-	},
-	{
-		company: "Samsung",
-		number: 4356637,
-		size: "5.0",
-		companyPartner: "SamMobile",
-		manufactured: new Date("2021-11-05"),
-	},
-	{
-		company: "Apple",
-		number: 4552833,
-		size: "5.7",
-		companyPartner: "no data",
-		manufactured: new Date("2022-05-24T12:00:00"),
-	},
+const phones: IMobilePhone[] = [
+  {
+    company: 'Nokia',
+    number: 1285637,
+    size: '5.5',
+    companyPartner: 'MobileNokia',
+    manufactured: new Date('2022-09-01'),
+  },
+  {
+    company: 'Samsung',
+    number: 4356637,
+    size: '5.0',
+    companyPartner: 'SamMobile',
+    manufactured: new Date('2021-11-05'),
+  },
+  {
+    company: 'Apple',
+    number: 4552833,
+    size: '5.7',
+    companyPartner: 'no data',
+    manufactured: new Date('2022-05-24T12:00:00'),
+  },
 ];
 
 interface IPhonesManufacturedAfterDate extends IMobilePhone {
-	initialDate: string;
+  initialDate: string;
 }
 
 // Функция должна отфильтровать массив данных и вернуть новый массив
 // с телефонами, выпущенными после даты в третьем аргументе
 
 function filterPhonesByDate(
-	phones: [],
-	key: string,
-	initial: string
-): IPhonesManufacturedAfterDate[] {}
+  phones: IMobilePhone[],
+  key: keyof IMobilePhone,
+  initial: string
+): IPhonesManufacturedAfterDate[] {
+  return phones
+    .filter((phone) => {
+      const manufactured = phone[key];
+      if (
+        manufactured instanceof Date &&
+        manufactured.getDate() > new Date(initial).getDate()
+      ) {
+        return phone;
+      }
+    })
+    .map((phone) => {
+      const newObj = { ...phone, initialDate: initial };
+      return newObj;
+    });
+}
 
 // Второй аргумент при вызове функции должен быть связан с первым,
 // а значит мы получим подсказки - свойства этого объекта
 
-console.log(filterPhonesByDate(phones, "manufactured", "2022-01-01"));
+console.log(filterPhonesByDate(phones, 'manufactured', '2022-01-01'));
