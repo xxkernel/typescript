ec5f09716b6632162aba6083b2e7158630ac671a
HiyoRitsu <122852098+HiyoRitsu@users.noreply.github.com>
2024-03-18 06:59:29 +0600

    tasks

--- course_materials/4 - Typescript Generics and type manipulations/61 - form.ts
+++ course_materials/4 - Typescript Generics and type manipulations/61 - form.ts
@@ -1,13 +1,17 @@
 interface IForm {
-	login: string;
-	password: string;
+  login: string;
+  password: string;
 }
 
 // Необходимо типизировать объект валидации
 // Учтите, что данные в форме могут расширяться и эти поля
 // должны появиться и в объекте валидации
 
-const validationData = {
-	login: { isValid: false, errorMsg: "At least 3 characters" },
-	password: { isValid: true },
+const validationData: Validation<IForm> = {
+  login: { isValid: false, errorMsg: 'At least 3 characters' },
+  password: { isValid: true },
+};
+
+type Validation<T> = {
+  [K in keyof T]: { isValid: true } | { isValid: false; errorMsg: string };
 };
